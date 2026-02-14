9dc59c50af0fe08b57d2543dea382911818577c5
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-14 14:25:17 +0400

    feat: getting countries

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Providers } from "./providers";
+import { Providers } from "./(common)/providers";
 
 export const metadata: Metadata = {
   title: "Where in the world?",
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+import { DataProvider } from "./data-provider";
+
 export default function Home() {
-  return <main></main>;
+  const [countries, setCountries] = useState<any[]>([]);
+
+  useEffect(() => {
+    const loadCountries = async () => {
+      const data = await DataProvider.getAllCountries();
+      setCountries(data);
+      console.log("useEffect");
+    };
+
+    loadCountries();
+  }, []);
+
+  return (
+    <main>
+      {countries.map((element: any) => {
+        console.log("entered");
+        return <div key={element.name.common}>{element.name.common}</div>;
+      })}
+    </main>
+  );
 }
