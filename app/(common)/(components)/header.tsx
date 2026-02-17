df2b7ac25a3ebe6b2f8b7ae012732a3970096536
Luka Gogichaishvili <luka595664@gmail.com>
2026-02-17 17:16:18 +0400

    feat: dark theme general

--- app/(common)/(components)/header.tsx
+++ app/(common)/(components)/header.tsx
@@ -1,9 +1,26 @@
+"use client";
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
 export default function Header() {
+  const [mounted, setMounted] = useState(false);
+  const { setTheme, resolvedTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  const switchTheme = () => {
+    if (resolvedTheme === "dark") {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  };
   return (
     <header>
       <p>Where in the world?</p>
-      {/* theme switcher placeholder */}
-      <button>Dark Mode</button>
+      <button className="clickable" onClick={() => switchTheme()}>
+        Dark Mode
+      </button>
     </header>
   );
 }
